3434a5df26fc0de6684952d5567e71cacced6c9e
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-11 11:26:43 +0200

    feat(settings): enable history and collections apps

--- apis_instance_gideon/settings.py
+++ apis_instance_gideon/settings.py
@@ -2,3 +2,5 @@ from apis_acdhch_default_settings.settings import *
 
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_instance_gideon"] + INSTALLED_APPS
+INSTALLED_APPS += ["apis_core.history"]
+INSTALLED_APPS += ["apis_core.collections"]
