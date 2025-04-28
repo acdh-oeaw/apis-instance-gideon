83aca38313b8b10127e6bcec744d4aecde47f9af
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-28 16:38:25 +0200

    fix(apps): drop default_auto_field override from AppConfig

--- apis_instance_gideon/apps.py
+++ apis_instance_gideon/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class ApisInstanceGideonConfig(AppConfig):
-    default_auto_field = "django.db.models.AutoField"
     name = "apis_instance_gideon"
 
     def ready(self):
