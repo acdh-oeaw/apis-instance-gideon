4f19dbe0c60570b299940e1b9c5865f2435ec8f1
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-30 10:10:06 +0200

    fix(settings): drop apps that are shipped with default settings

--- apis_instance_gideon/settings.py
+++ apis_instance_gideon/settings.py
@@ -2,7 +2,5 @@ from apis_acdhch_default_settings.settings import *
 
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_instance_gideon"] + INSTALLED_APPS
-INSTALLED_APPS += ["apis_core.history"]
-INSTALLED_APPS += ["apis_core.collections"]
 
 STATIC_ROOT = "/attachments/static"
