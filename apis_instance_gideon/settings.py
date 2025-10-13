ed441224fecd2437500ba12fba33b3f97fa92b2b
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-10-13 08:25:34 +0200

    fix(settings): adapt STATIC_ROOT path to new `/data` volume

--- apis_instance_gideon/settings.py
+++ apis_instance_gideon/settings.py
@@ -3,4 +3,4 @@ from apis_acdhch_default_settings.settings import *
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_instance_gideon"] + INSTALLED_APPS
 
-STATIC_ROOT = "/attachments/static"
+STATIC_ROOT = "/data/static"
