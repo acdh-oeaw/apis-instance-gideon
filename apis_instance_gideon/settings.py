ad91f6ab8c1a4c11c04acea0f3fc2c09e5a3ea18
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-05-22 07:43:56 +0200

    feat(settings): add `django.contrib.postgres` to INSTALLED_APPS

--- apis_instance_gideon/settings.py
+++ apis_instance_gideon/settings.py
@@ -2,5 +2,6 @@ from apis_acdhch_default_settings.settings import *
 
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_instance_gideon"] + INSTALLED_APPS
+INSTALLED_APPS += ["django.contrib.postgres"]
 
 STATIC_ROOT = "/data/static"
